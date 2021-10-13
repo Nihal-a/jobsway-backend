d4c83a265ba22465e24709718bfebe244901b1b0
Nihal-a <nihalavulan1@gamil.com>
2021-10-13 17:15:12 +0530

    Admin service routes and controller structure created

--- admin-service/index.js
+++ admin-service/index.js
@@ -1,7 +1,10 @@
 import express from 'express'
+import routes from './routes/routes.js'
 
 const PORT  = 3000;
 const app = express()
+
+app.use('/',routes)
 
 app.listen(PORT,(err) => {
     if(err) console.log("Server failed to start. Error : " + err);
