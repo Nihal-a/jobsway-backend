dfead87edb81addb7a1ff1ef40ec8e56e447ee1b
Nihal-a <nihalavulan1@gamil.com>
2021-10-14 12:11:11 +0530

    user-service database connected

--- user-service/index.js
+++ user-service/index.js
@@ -1,11 +1,16 @@
 var express = require('express')
 var routes = require('./routes/routes')
-
+var db = require('./config/connection')
 
 const PORT  = 8000;
 const app = express()
 
 app.use('/',routes)
+
+db.connect((err)=>{
+    if(err) console.log("Database Connection Error"+err);
+    else console.log("database Connected Successfully");
+})
 
 app.listen(PORT,(err) => {
     if(err) console.log("Server failed to start. Error : " + err);
