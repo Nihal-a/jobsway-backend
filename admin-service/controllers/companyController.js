7e652e572017d259fc64a7290f92cd2b638bfb61
Nihal-a <nihalavulan1@gamil.com>
2021-11-01 11:11:05 +0530

    company rejection completed

--- admin-service/controllers/companyController.js
+++ admin-service/controllers/companyController.js
@@ -16,6 +16,19 @@ module.exports = {
             res.status(400).json(error)
         }
     },
+    getVerifiedCompanies :async (req,res) => {
+        console.log("REached---------------------------");
+        try {
+            var verifiedCompanies = await db.get().collection(collection.COMPANY_COLLECTION).find({status : true}).toArray()
+
+            console.log(verifiedCompanies);
+            res.status(200).json(verifiedCompanies)
+
+        } catch (error) {
+            console.log(error);
+            res.status(400).json(error)
+        }
+    },
     verifyCompany : async(req,res) => {
         var id = req.query.id  
         try {
@@ -29,5 +42,21 @@ module.exports = {
             console.log(error);
             res.status(400).json(error)
         }
-    }
+    },
+    rejectCompany : async(req,res) => {
+        var id = req.query.id  
+        var {reason} = req.body
+        try {
+            var rejectCompany = await db.get().collection(collection.COMPANY_COLLECTION).updateOne({_id:ObjectId(id)},{
+                $set : {
+                    status : "Rejected",
+                    reason : reason
+                }
+            })
+            res.status(200).json(rejectCompany)
+        } catch (error) {
+            console.log(error);
+            res.status(400).json(error)
+        }
+    },
 }
--- admin-service/routes/routes.js
+++ admin-service/routes/routes.js
@@ -1,12 +1,14 @@
 var express = require('express')
 var { signin} = require('../controllers/authControllers')
-var {getUnVerifiedCompanies,verifyCompany} = require('../controllers/companyController')
+var {getUnVerifiedCompanies,verifyCompany,getVerifiedCompanies,rejectCompany} = require('../controllers/companyController')
 
 const router  = express.Router();
 
 //company routes
 router.get('/', getUnVerifiedCompanies)
+router.get('/companies', getVerifiedCompanies)
 router.patch('/',verifyCompany)
+router.patch('/reject',rejectCompany)
 
 
 //Auth routes
