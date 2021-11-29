0375043c824526ec3ece155f7315d4fe4f9e38fa
Nihal-a <nihalavulan1@gamil.com>
2021-11-30 00:24:19 +0530

    Delete job Successfully

--- company-service/routes/routes.js
+++ company-service/routes/routes.js
@@ -1,8 +1,9 @@
 var express = require('express');
-const {registerCompany, loginCompany,reregisterCompany} = require('../controllers/Auth');
-const { getCompanyDetails ,  addJob ,addJobPayment ,verifyPayment, addFreeJob,getCompanyJobs ,addTransaction,stripePayment ,getJobById} = require('../controllers/Company');
-const { validateCompanyRegistration, valdiateJobDetails } = require('../middlewares/JobVerification');
-
+const {registerCompany , reregisterCompany , loginCompany} = require('../controllers/Auth');
+const {getCompanyDetails , getCompanyJobs } = require('../controllers/Company');
+const { getJobById , addJob , addFreeJob , deleteJob} = require('../controllers/Jobs');
+const { addTransaction , addJobPayment , verifyPayment , stripePayment } = require('../controllers/payments');
+const {valdiateJobDetails , validateCompanyRegistration } = require('../middlewares/JobVerification')
 
 const router  = express.Router();
 
@@ -14,20 +15,21 @@ router.patch('/reregister', reregisterCompany)
 
 //Company
 router.get('/company/:id', getCompanyDetails)
+router.get('/jobs/:id' , getCompanyJobs)
 
 //Jobs
 router.post('/add-job' ,valdiateJobDetails,addJob)
-router.get('/jobs/:id' , getCompanyJobs)
 router.get('/job/:id' , getJobById)
-
-
+router.delete('/delete-job/:id' , deleteJob)
+router.post('/add-free-plan', addFreeJob)
 
 
 //Payment
+
+        //Razorpay
         router.post('/add-transaction',addTransaction)
         router.post('/addjobpayment', addJobPayment)
         router.post('/verify-payment', verifyPayment)
-        router.post('/add-free-plan', addFreeJob)
         
         //Stripe
         router.post('/create-payment-intent' , stripePayment)
